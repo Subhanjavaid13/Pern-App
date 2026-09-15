d29cbf2c3cb42db3cf604db250c064dbd8fbb205
Muhammad subhan <subhanjavaid5616@gmail.com>
2026-09-15 17:28:05 +0500

    feat: implement routing and add HomePage, ProductPage, and NavBar components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
+import NavBar from './components/NavBar'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import ProductPage from './pages/ProductPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <>
-   <h1 className="bg-blue-200 text-center">Hello Frontend</h1>
-   </>
+  <div className='min-h-screen bg-base-200 transition-colors duration-300' data-theme="dark">
+    <NavBar/>
+    <Routes>
+      <Route path='/' element={<HomePage/>}/>
+      <Route path='/product/:id' element={<ProductPage/>}/>
+    </Routes>
+  </div>
   )
 }
 
---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className="bg-base-100/80 backdrop:blur-lg border-b border-base-content/10 sticky top-0 z-10 ">
+      <div className="max-w-7xl mx-auto">
+        <div className="navbar px-4 min-h-[4rem] justify-between">
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
