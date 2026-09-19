74d172bc7f521dbbfa952c96e9435c90e3bb73ea
Muhammad subhan <subhanjavaid5616@gmail.com>
2026-09-19 21:36:45 +0500

    feat: add product store with fetchProducts functionality using Zustand

---
+++ frontend/src/store/useProductStore.js
@@ -0,0 +1,22 @@
+import {create} from 'zustand';
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:3000';
+export const useProductStore = create((set, get) => ({
+    products: [],
+    loading:false,
+    error:null,
+
+    fetchProducts: async () => {
+        set({ loading: true});
+        try{
+            const res = await axios.get(`${BASE_URL}/api/products`);
+            set({ products: res.data.data, error:null });
+        }catch(error){
+            if(error.status === 429) set({error: "Too many requests. Please try again later."});
+            else set({error: error.message});
+        }finally{
+            set({loading:false});
+        }
+    }
+}))
