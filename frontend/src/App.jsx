01657fbb2743dc9d22b3669aef09453772026f65
Muhammad subhan <subhanjavaid5616@gmail.com>
2026-09-19 20:58:52 +0500

    feat: add theme management with Zustand store and integrate into App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,13 @@ import NavBar from './components/NavBar'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import ProductPage from './pages/ProductPage'
+import { useThemeStore } from './store/useThemeStore'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const {theme, setTheme} = useThemeStore();
   return (
-  <div className='min-h-screen bg-base-200 transition-colors duration-300' data-theme="dark">
+  <div className='min-h-screen bg-base-200 transition-colors duration-300' data-theme={theme}>
     <NavBar/>
     <Routes>
       <Route path='/' element={<HomePage/>}/>
